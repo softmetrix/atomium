dc8f7f0ce45d963a5d0d02a5cbd7d25c394dc3ca
Marko Medojevic <mmedojevicbg@gmail.com>
2019-06-23 16:59:59 +0200

    installation script

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -9,13 +9,40 @@ class BaseController extends Controller
 {
     public function beforeAction($action)
     {
+        $this->installation($action);
+        $this->authentication($action);
+
+        return parent::beforeAction($action);
+    }
+
+    private function authentication($action)
+    {
         $loginAction = '/auth/login';
-        $currentAction = '/' . $action->controller->id . '/' . $action->id;
-        if ($currentAction != $loginAction) {
+        $installAction = '/install/index';
+        $currentAction = '/'.$action->controller->id.'/'.$action->id;
+        if ($currentAction != $loginAction
+            && $currentAction != $installAction) {
             if (!Yii::$app->user->identity) {
                 $this->redirect($loginAction);
+                Yii::$app->end();
             }
         }
-        return parent::beforeAction($action);
+    }
+
+    private function installation($action)
+    {
+        if (!$this->isInstalled()) {
+            $installAction = '/install/index';
+            $currentAction = '/'.$action->controller->id.'/'.$action->id;
+            if ($currentAction != $installAction) {
+                $this->redirect($installAction);
+                Yii::$app->end();
+            }
+        }
+    }
+
+    protected function isInstalled()
+    {
+        return Yii::$app->db->getTableSchema('pje_execution_step', true) !== null;
     }
 }
---
+++ controllers/InstallController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\controllers;
+
+class InstallController extends BaseController
+{
+    public function actionIndex()
+    {
+        $this->layout = false;
+        if ($this->isInstalled()) {
+            return $this->redirect('/');
+        }
+        $basePath = \Yii::$app->basePath;
+        $retVal = null;
+        echo '<pre>';
+        passthru("cd {$basePath}; ./install", $retVal);
+        echo '</pre>';
+        if ($retVal === 0) {
+            echo 'INSTALLED<br />';
+            echo '<a href="/">Go to application</a>';
+        } else {
+            echo 'Something went wrong';
+        }
+    }
+}
