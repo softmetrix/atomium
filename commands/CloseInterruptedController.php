d46fdd29402a9b00572db6efb1177a8368ddef0b
Marko Medojevic <mmedojevicbg@gmail.com>
2020-01-09 22:43:47 +0100

    close interrupted win support

--- commands/CloseInterruptedController.php
+++ commands/CloseInterruptedController.php
@@ -1,8 +1,8 @@
 <?php
+
 namespace app\commands;
 
 use yii\console\Controller;
-use Yii;
 use app\models\PjeExecution;
 use app\models\PjeExecutionStep;
 use app\models\PjeNotification;
@@ -11,47 +11,45 @@ class CloseInterruptedController extends Controller
 {
     public function actionIndex()
     {
-        if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
-            $inProgress = PjeExecution::find()->where([
-                'end_time' => null
+        $inProgress = PjeExecution::find()->where([
+                'end_time' => null,
             ])->all();
-            foreach ($inProgress as $item) {
-                if ($item->pid && !file_exists("/proc/{$item->pid}")) {
-                    $execution = PjeExecution::find()->where([
-                        'id' => $item->id
+        foreach ($inProgress as $item) {
+            if ($item->pid && !$this->pidIsRunning($item->pid)) {
+                $execution = PjeExecution::find()->where([
+                        'id' => $item->id,
                     ])->one();
-                    if (!$execution->end_time) {
-                        PjeExecutionStep::updateAll([
+                if (!$execution->end_time) {
+                    PjeExecutionStep::updateAll([
                             'start_time' => date('Y-m-d H:i:s'),
                             'end_time' => date('Y-m-d H:i:s'),
                             'duration' => 0,
                             'success' => 0,
-                            'response_message' => 'Interrupted'
+                            'response_message' => 'Interrupted',
                         ], 'end_time is null');
-                        $duration = PjeExecutionStep::find()
+                    $duration = PjeExecutionStep::find()
                                                     ->where(['execution_id' => $execution->id])
                                                     ->andWhere(['not', ['duration' => null]])
                                                     ->sum('duration');
-                        $startTime = PjeExecutionStep::find()
+                    $startTime = PjeExecutionStep::find()
                                                     ->where(['execution_id' => $execution->id])
                                                     ->andWhere(['not', ['start_time' => null]])
                                                     ->min('start_time');
-                        $endTime = PjeExecutionStep::find()
+                    $endTime = PjeExecutionStep::find()
                                                     ->where(['execution_id' => $execution->id])
                                                     ->andWhere(['not', ['end_time' => null]])
                                                     ->max('end_time');
-                        $execution->duration = $duration;
-                        $execution->success = 0;
-                        $execution->start_time = $startTime;
-                        $execution->end_time = $endTime;
-                        $execution->save(false);
-                        $this->generateNotification($execution);
-                    }
+                    $execution->duration = $duration;
+                    $execution->success = 0;
+                    $execution->start_time = $startTime;
+                    $execution->end_time = $endTime;
+                    $execution->save(false);
+                    $this->generateNotification($execution);
                 }
             }
         }
     }
-    
+
     protected function generateNotification($execution)
     {
         $notification = new PjeNotification();
@@ -60,5 +58,27 @@ class CloseInterruptedController extends Controller
         $notification->notification_date = $execution->end_time;
         $notification->message = 'Job interrupted';
         $notification->save();
+    }
+
+    protected function pidIsRunning($pid)
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+            $running = file_exists("/proc/{$pid}");
+        } else {
+            $taskList = shell_exec('tasklist 2>NUL');
+            $taskTable = explode("\n", trim($taskList));
+            $delimiters = explode(' ', $taskTable[1]);
+            $pidColumnStart = strlen($delimiters[0]) + 1;
+            $pidColumnLength = strlen($delimiters[1]);
+            $pids = [];
+            foreach ($taskTable as $key => $task) {
+                if ($key > 1) {
+                    $pids[] = (int) substr($task, $pidColumnStart, $pidColumnLength);
+                }
+            }
+            $running = in_array($pid, $pids);
+        }
+
+        return $running;
     }
 }
